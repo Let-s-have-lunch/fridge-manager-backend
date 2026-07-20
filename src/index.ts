9f7ada2d6c67e1de7a0a01f117588d72b2d84091
DeveloperSky95 <haneulgim95@gmail.com>
2026-07-20 14:45:51 +0900

    프로젝트 준비

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || "8080";
+
+app.use(
+    cors({
+        origin: ["http://localhost:8081", "http://localhost:8082", "http://localhost:8083"],
+        credentials: true,
+    }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`서버 실행됨! http://localhost:${PORT}`);
+});
