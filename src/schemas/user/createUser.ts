f4bf2e6589aaa6f19d327a91b630bc18e0d04028
yeonhan <yeonhanlab@gmail.com>
2026-07-29 18:01:13 +0900

    오류 수정

--- src/schemas/user/createUser.ts
+++ src/schemas/user/createUser.ts
@@ -4,12 +4,7 @@ export const createUserSchema = z.object({
     nickname: z.string().min(2).max(10),
     password: z.string().min(6),
     email: z.email(),
-    birthdate: z
-        .string()
-        .regex(/^\d{8}$/)
-        .transform(
-            value => new Date(`${value.slice(0, 4)}-${value.slice(4, 6)}-${value.slice(6, 8)}`),
-        ),
+    birthdate: z.string().optional(),
 });
 
 export type CreateUserInputType = z.infer<typeof createUserSchema>;
